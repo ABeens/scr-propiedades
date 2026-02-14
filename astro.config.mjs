44133215fd748d2a0a2124ee1442df5b283447ac
Abraham <Abrham_beens_90@hotmail.com>
2026-02-14 02:27:42 -0600

    ajustede rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://abeens.github.io',
+  base: '/scr-propiedades',
 });
